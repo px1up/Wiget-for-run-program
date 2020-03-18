90dc2aee9e15b3c26bcf2bb17348c977afa3ede5
Vladimir Yakovlev <40215129+px1up@users.noreply.github.com>
2020-03-18 22:30:57 +0300

    Add files via upload

--- wiget.py
+++ wiget.py
@@ -1,15 +1,22 @@
 import psutil
 from tkinter import *
 
-def window():
+programm =str("program.exe")
+
+def message_window():
     window = Tk()
     window.title("")
+    window.geometry('250x150')
+    lbl = Label( width=30, height=10, text="message",font=("Arial Bold", 10))
+    lbl.grid(column=0, row=0)
     window.mainloop()
+
 
 num = 1
 while num == 1:
     for process in psutil.process_iter():
-        if process.name() == 'notepad.exe':
-            window()
-if window():
-    exit(0)
+        if process.name() == programm:
+            num = 2
+        else:
+            num == 2
+message_window()
